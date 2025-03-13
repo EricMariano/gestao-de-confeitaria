3c154c0b8675582e1d775f32e8167e84edd9ee8e
EricMariano <ericbfmariano@gmail.com>
2025-03-13 20:54:36 -0300

    add: Card do menu do perfil e adicionando o react router ao json

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 export default function Avatar() {
-    return(
-        <div className="btn btn-circle bg-primary-site border-none mr-4 w-8.5 h-8.5">
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="dropdown dropdown-end relative">
+        <input type="checkbox" checked={isOpen} readOnly hidden />
+        <div
+            className=" btn btn-ghost btn-circle bg-primary-site border-none mr-4 w-8.5 h-8.5"
+            onClick={() => setIsOpen(!isOpen)}
+            tabIndex={0}
+        >
             <span className="text-xs">AC</span>
-        </div>
-    )
+      </div>
+        
+        {/* Card */}
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <li>
+            <a className="justify-between">
+              Profile
+            </a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            <a>Logout</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
